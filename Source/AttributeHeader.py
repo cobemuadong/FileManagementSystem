3a8217abd76319a18373a53df029aecc1b83a9bb
Trung <trung22112002@gmail.com>
2022-10-16 17:49:39 +0700

    Sua loi chinh ta

--- Source/AttributeHeader.py
+++ Source/AttributeHeader.py
@@ -1,9 +1,9 @@
 class AttributeHeader:
-    def __init__(self, type, length, resistent_flag, name_length, name_offset, flags, 
+    def __init__(self, type, length, resident_flag, name_length, name_offset, flags, 
     attribute_id, length_of_attribute, offset_to_attribute, indexed_flag) -> None:
         self.type = type
         self.length = length
-        self.resistent_flag = resistent_flag
+        self.resistent_flag = resident_flag
         self.name_length = name_length
         self.name_offset = name_offset
         self.flags = flags
